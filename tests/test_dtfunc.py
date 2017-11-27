08552b0e7e9868d0280779dca6959e3fe223b757
Ryan Ellis <ryan.ellis@va.gov>
2017-11-27 14:05:49 -0800

    better tests

--- tests/test_dtfunc.py
+++ tests/test_dtfunc.py
@@ -24,34 +24,29 @@ class Test(unittest.TestCase):
 
         dat_3d_square = np.random.rand(30, 30, 30)
         dat_3d_not_square = np.random.rand(30, 30, 31)
-        dat_4d_square = np.random.rand(30, 30, 30, 30)
-        dat_4d_not_square = np.random.rand(30, 30, 30, 31)
 
         self.assertEqual(dtfunc.denoise(dat_3d_square).shape, dat_3d_square.shape)
         self.assertEqual(dtfunc.denoise(dat_3d_not_square).shape, dat_3d_not_square.shape)
-        self.assertEqual(dtfunc.denoise(dat_4d_square).shape, dat_4d_square.shape)
-        self.assertEqual(dtfunc.denoise(dat_4d_not_square).shape, dat_4d_not_square.shape)
 
     def test_b0_avg(self):
-        dat = np.random.rand(96, 96, 59, 54)
-        aff = np.random.rand(4, 4)
-        b0 = np.zeros(1)
-
-        self.assertEqual(dtfunc.b0_avg(dat, aff, b0).shape, (96, 96, 59))
+        img = nib.load('/home/vhasfcellisr/Ryan/test/t4/test4.nii.gz')
+        bval, bvec = read_bvals_bvecs('/home/vhasfcellisr/Ryan/test/t4/test4.bval', None)
+        dat = img.get_data()
+        aff = img.affine
+        b0 = dtfunc.b0_avg(dat, aff, bval)
+        self.assertEqual(b0.shape, dat[..., 0].shape)
 
     def test_register(self):
 
-        img = nib.load('/home/vhasfcellisr/Ryan/test/t1/test1.nii.gz')
-        bval, bvec = read_bvals_bvecs('/home/vhasfcellisr/Ryan/test/t1/test1.bval',
-                                      '/home/vhasfcellisr/Ryan/test/t1/test1.bvec')
+        img = nib.load('/home/vhasfcellisr/Ryan/test/t4/test4.nii.gz')
+        bval, bvec = read_bvals_bvecs('/home/vhasfcellisr/Ryan/test/t4/test4.bval',
+                                      '/home/vhasfcellisr/Ryan/test/t4/test4.bvec')
         dat = img.get_data()
         aff = img.affine
 
-        shape = dat.shape
-        b0 = np.random.rand(shape[0], shape[1], shape[2])
-        b0_aff = np.random.rand(4, 4)
+        b0 = dtfunc.b0_avg(dat, aff, bval)
 
-        reg_dat, reg_bvecs = dtfunc.register(b0, dat, b0_aff, aff, bval, bvec)
+        reg_dat, reg_bvecs = dtfunc.register(b0, dat, aff, aff, bval, bvec)
 
         self.assertEqual(dat.shape, reg_dat.shape)
 
