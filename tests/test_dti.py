29c5a89bbfc85deb3eed809539c2a24475230dfa
Ryan Ellis <ryan.ellis@va.gov>
2017-11-28 17:17:06 -0800

    qc 'test'

--- tests/test_dti.py
+++ tests/test_dti.py
@@ -1,12 +1,27 @@
 import unittest
-from pijp_dti import dti
+
+import matplotlib.pyplot as plt
+import nibabel as nib
+import numpy as np
+
 
 class Test(unittest.TestCase):
 
     def test_MaskQC(self):
 
-        qc = dti.MaskQC(None, None, None)
-        qc.fdwi = '/home/vhasfcellisr/Ryan/test_dti/subjects/t1/stage/t1.nii.gz'
-        qc.prereg = '/home/vhasfcelilsr/Ryan/test_dti/subjects/t1/prereg/t1_prereg.nii.gz'
+        og = nib.load('/home/vhasfcellisr/Ryan/test_dti/subjects/t5/stage/t5.nii.gz')
+        mask = nib.load('/home/vhasfcellisr/Ryan/test_dti/subjects/t5/prereg/t5_prereg.nii.gz')
+        og = og.get_data()
+        mask = mask.get_data()
+        mask = np.where(mask > 0, 0, 1)
+        fig, ax = plt.subplots(3, 3)
 
-        qc.run()
+        for i in range(0, 3):
+            ax[0, i].imshow(og[:, :, og.shape[2]//4, i], cmap='nipy_spectral')
+            ax[0, i].imshow(mask[:, :, mask.shape[2]//4, i], cmap='binary_r', alpha=0.5)
+            ax[1, i].imshow(og[:, :, og.shape[2]//2, i], cmap='nipy_spectral')
+            ax[1, i].imshow(mask[:, :, mask.shape[2]//2, i], cmap='binary_r', alpha=0.5)
+            ax[2, i].imshow(og[:, :, og.shape[2]*3//4, i], cmap='nipy_spectral')
+            ax[2, i].imshow(mask[:, :, mask.shape[2]*3//4, i], cmap='binary_r', alpha=0.5)
+
+        plt.show()
