5ec88b2d25030e115730a3fa3dd07459e1872925
Ryan Ellis <ryan.ellis@va.gov>
2017-12-01 17:18:05 -0800

    testing mosaic

--- tests/test_dti.py
+++ tests/test_dti.py
@@ -7,18 +7,15 @@ import numpy as np
 from dipy.segment.mask import applymask
 
 from pijp_dti import dtfunc
-from pijp_dti import animate
+from pijp_dti import visuals
 
 class Test(unittest.TestCase):
 
     def test_MaskQC(self):
-
         og = nib.load('/home/vhasfcellisr/Ryan/t1_dti/subjects/test2/stage/test2.nii.gz').get_data()
         mask = dtfunc.mask(og)
 
-        masked = animate.mask_image(og, mask, hue = 0, alpha=0.9)
-        mov = animate.Nifti_Animator(masked)
-        mov.cmap = "hot"
-        mov.interval = 10
-        mov.plot()
-
+        masked = visuals.mask_image(og, mask, hue = 0, alpha=0.9)
+        mos = visuals.Mosaic(masked[..., 0])
+        mos.cmap = "hot"
+        mos.plot(show=False, save=False, path= '/home/vhasfcellisr/test.png')
