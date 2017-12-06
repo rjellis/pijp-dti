e80c5915c0df23b7cc836a39b88a5a8a7f30f094
Ryan Ellis <ryan.ellis@va.gov>
2017-12-06 10:20:22 -0800

    testing mask function

--- tests/test_dtfunc.py
+++ tests/test_dtfunc.py
@@ -1,16 +1,35 @@
 import unittest
 
+import numpy as np
 import nibabel as nib
 from dipy.io import read_bvals_bvecs
 
 from pijp_dti import dtfunc
+from pijp_dti import dtiQC
 
 
 class Test(unittest.TestCase):
 
+    def test_mask(self):
+        img = nib.load('/home/vhasfcellisr/Ryan/test/t2/test2.nii.gz')
+        dat = img.get_data()
+        # dat = dtfunc.denoise(dat)
+        b0 = dat[..., 0]
+        b0 = dtiQC.rescale(b0)
+        b0 = np.stack((b0, b0, b0), axis=-1)
+
+        mask = dtfunc.mask(dat)
+        mask = mask[..., 0]
+
+        masked = dtiQC.mask_image(b0.astype(float), mask, hue=[0, 1, 0], alpha=0.7)
+
+        fig = dtiQC.Nifti_Animator(masked)
+        fig.plot()
+
+
     def test_denoise(self):
 
-        img = nib.load('/home/vhasfcellisr/Ryan/test/t2/test2.nii.gz')
+        img = nib.load('/home/vhasfcellisr/Ryan/test/t1/test1.nii.gz')
 
         dat = img.get_data()
 
