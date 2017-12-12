e70463d25e25958b1e4900d125feaeb1dc1c25e8
Ryan Ellis <ryan.ellis@va.gov>
2017-12-11 17:12:02 -0800

    added get_step_queue and get_mask_qc_list

--- pijp_dti/repository.py
+++ pijp_dti/repository.py
@@ -7,6 +7,65 @@ class DTIRepository(BaseRepository):
         super().__init__()
         self.connection.setdb('imaging')
 
+    def get_step_queue(self, project, process, step):
+            sql = r"""
+            SELECT 
+            ScanCode AS Code
+            FROM 
+            ProcessingLog pl 
+            WHERE Project = {project} 
+            AND Process = {process}
+            AND Step = {prev_step_name}
+            AND (Outcome = 'Done' OR Outcome = 'pass')
+            AND ScanCode NOT IN (SELECT 
+            ScanCode
+            FROM 
+            ProcessingLog pl 
+            WHERE Project = {project} 
+            AND Process = {process} 
+            AND Step = {step_name}
+            AND (Outcome = 'Done' OR Outcome = 'pass'))
+            
+            """.format(project=dbprocs.format_string_parameter(project),
+                       process=dbprocs.format_string_parameter(process),
+                       step_name=dbprocs.format_string_parameter(step.step_name),
+                       prev_step_name=dbprocs.format_string_parameter(step.prev_step.step_name))
+
+            ready = self.connection.fetchall(sql)
+            return ready
+
+    def get_mask_qc_list(self, project):
+        sql = r"""
+        SELECT 
+            ScanCode AS Code
+        FROM 
+            ProcessingLog pl 
+        WHERE 
+            Project = {0}
+        AND 
+            Process = 'dti' 
+        AND 
+            Step = 'Preregister'
+        AND 
+            Outcome = 'Done'
+        AND 
+            ScanCode NOT IN(
+                SELECT 
+                    ScanCode
+                FROM 
+                    ProcessingLog pl 
+                WHERE Project = {0} 
+                    AND Process = 'dti' 
+                AND 
+                    Step = 'MaskQC'
+                AND 
+                    Outcome = 'pass'
+            )
+        """.format(dbprocs.format_string_parameter(project))
+
+        todo = self.connection.fetchall(sql)
+        return todo
+
     def get_project_dtis(self, project):
 
         sql = r"""
