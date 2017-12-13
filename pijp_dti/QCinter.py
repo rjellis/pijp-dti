2a5e257e8e2f21b56593de6dac4e132ce6cd3c83
Ryan Ellis <ryan.ellis@va.gov>
2017-12-12 16:10:22 -0800

    some gui changes

--- pijp_dti/QCinter.py
+++ pijp_dti/QCinter.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -13,31 +14,43 @@ class Application(tk.Frame):
         master.columnconfigure(0, weight=1)
         master.rowconfigure(0, weight=1)
         master.geometry("1280x720")
+        master.configure(bg='black')
         self.config(bg='black')
-        self.grid(rowspan=4, columnspan=4, stick="news")
+        self.grid(rowspan=4, columnspan=4, stick="news", padx=5, pady=5)
+
 
     def create_widgets(self):
         self.winfo_toplevel().title("QC Tool")
+        self.bind("<ButtonPress-1>", self.StartMove)
+        self.bind("<ButtonRelease-1>", self.StopMove)
+        self.bind("<B1-Motion>", self.OnMotion)
+
         self.button_quit = tk.Button(master=self.master, text='Exit', bg='white', fg='black', command=self._quit)
         self.button_pass = tk.Button(master=self.master, text='Pass', command=self._pass, highlightcolor='green',
                                      bg='white', fg='black')
         self.button_fail = tk.Button(master=self.master, text='Fail', command=self._fail, highlightcolor='red',
                                      bg='white', fg='black')
-        self.button_comment = tk.Button(master=self.master, text='Save Comment', command=self._save_comment,
+        self.button_comment = tk.Button(master=self.master, text='Save Comment', command=self.save_comment,
                                         bg='white', fg='black')
 
         v = tk.StringVar()
         self.entry_comment = tk.Entry(master=self.master, width=100, textvariable=v)
         self.label_comment = tk.Label(master=self.master, text="Comment: ", bg='black', fg='white')
-        self.label_top = tk.Label(master=self.master, text =self.code, bg='black', fg='white')
-
-        self.label_top.grid(column=0, row=0, sticky='n')
+        self.label_top = tk.Label(master=self.master, text =self.code, bg='black', fg='white', font=16)
+        self.label_top.bind("<ButtonPress-1>", self.StartMove)
+        self.label_top.bind("<ButtonRelease-1>", self.StopMove)
+        self.label_top.bind("<B1-Motion>", self.OnMotion)
+        
+        ttk.Sizegrip(self.master).grid(column=999, row=999, sticky='se')
+        self.label_top.grid(column=0, row=0, sticky='n', columnspan=4, rowspan=1)
         self.label_comment.grid(column=0, row=2, sticky='e')
         self.entry_comment.grid(column=1, row=2, columnspan=2, sticky='w', padx=5, pady=2)
+        self.entry_comment.bind("<Return>", self.save_comment)
         self.button_comment.grid(column=3, row=2, sticky='news', padx=5, pady=2)
         self.button_fail.grid(column=1, row=3, sticky='news', padx=5, pady=2)
         self.button_pass.grid(column=2, row=3, stick='news', padx=5, pady=2)
         self.button_quit.grid(column=3, row=3, sticky='news', padx=5, pady=2)
+
 
         for i in range(0, 3):
             self.columnconfigure(i, weight=1)
@@ -67,13 +80,32 @@ class Application(tk.Frame):
         self.button_pass.config(bg='white', fg='black')
         self.result = 'fail'
 
-    def _save_comment(self):
-        self.button_comment.config(text='Saved!', bg='green', fg='white')
+    def save_comment(self):
+        self.button_comment.config(text='   Saved!   ', bg='green', fg='white')
         self.comment = self.entry_comment.get()
 
+    def reset_comment_button(self):
+        self.button_comment.config(text='Save Comment', bg='white', fg='black')
+
+    def StartMove(self, event):
+        self.x = event.x
+        self.y = event.y
+
+    def StopMove(self, event):
+        self.x = None
+        self.y = None
+
+    def OnMotion(self, event):
+        deltax = event.x - self.x
+        deltay = event.y - self.y
+        x = self.master.winfo_x() + deltax
+        y = self.master.winfo_y() + deltay
+        self.master.geometry("+%s+%s" % (x, y))
 
 def run_qc_interface(figure, code):
     root = tk.Tk()
+    root.attributes('-topmost', True)
+    root.overrideredirect(True)
     app = Application(master=root)
     app.code = code
     app.create_widgets()
