fb158326fe94bef3bdfc5eac1525824de59b2f2c
Ryan Ellis <ryan.ellis@va.gov>
2017-12-12 16:10:39 -0800

    test for mask_qc updated

--- tests/test_dti.py
+++ tests/test_dti.py
@@ -1,22 +1,10 @@
 import unittest
-import os
-
-import matplotlib.pyplot as plt
-import nibabel as nib
-import numpy as np
-from dipy.segment.mask import applymask
-
-from pijp_dti import dtfunc
 from pijp_dti import dtiQC
 
 class Test(unittest.TestCase):
 
     def test_MaskQC(self):
-        og = nib.load('/home/vhasfcellisr/Ryan/t1_dti/subjects/test2/stage/test2.nii.gz').get_data()
-        mask = dtfunc.mask(og)
-
-        og = dtiQC.rescale(og)
-        og = np.stack((og, og, og), axis=-1)
-        masked = dtiQC.mask_image(og, mask, hue = [0, 1, 0], alpha=0.5)
-        mos = dtiQC.Mosaic(masked)
-        mos.plot(show=True, save=False, path= '/home/vhasfcellisr/test.png')
+        img = '/m/InProcess/External/NRC/dti/NRC-FRA018-0005-V0-a1001/prereg/NRC-FRA018-0005-V0-a1001_denoised.nii.gz'
+        msk = '/m/InProcess/External/NRC/dti/NRC-FRA018-0005-V0-a1001/prereg/NRC-FRA018-0005-V0-a1001_binary_mask.npy'
+        code = 'NRC-FRA018-0005-V0-a1001'
+        dtiQC.run_mask_qc(img, msk, code)
