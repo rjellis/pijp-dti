f4186b97cb30c419c7208c5d1d749cca42f1bf0e
Ryan Ellis <ryan.ellis@va.gov>
2017-12-19 13:55:02 -0800

    added header row to csv files, also added median measurement

--- pijp_dti/dtfunc.py
+++ pijp_dti/dtfunc.py
@@ -171,7 +171,7 @@ def roi_stats(dat, overlay, labels):
 
     """
     intensities_by_roi = dict()
-    stats = []
+    stats = [['name', 'min', 'max', 'mean', 'median', 'std. dev']]
 
     for roi_labels in labels.values():
         intensities_by_roi[roi_labels] = []
@@ -185,7 +185,7 @@ def roi_stats(dat, overlay, labels):
 
     for rois in intensities_by_roi.keys():
         npa = np.asarray(intensities_by_roi[rois])
-        stats.append([rois, npa.min(), npa.max(), npa.mean(), npa.std()])
+        stats.append([rois, npa.min(), npa.max(), npa.mean(), np.median(npa), npa.std()])
 
     return stats
 
