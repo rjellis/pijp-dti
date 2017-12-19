c42da6f0624aec6368eb7dc6b93145b09bdfdadb
Ryan Ellis <ryan.ellis@va.gov>
2017-12-19 15:46:46 -0800

    cleaned up SQL format

--- pijp_dti/repository.py
+++ pijp_dti/repository.py
@@ -12,21 +12,21 @@ class DTIRepository(BaseRepository):
     def get_step_queue(self, project, process, step):
             sql = r"""
             SELECT 
-            ScanCode AS Code
+                ScanCode AS Code
             FROM 
-            ProcessingLog pl 
+                ProcessingLog pl 
             WHERE Project = {project} 
-            AND Process = {process}
-            AND Step = {prev_step_name}
-            AND (Outcome = 'Done' OR Outcome = 'pass')
-            AND ScanCode NOT IN (SELECT 
-            ScanCode
-            FROM 
-            ProcessingLog pl 
-            WHERE Project = {project} 
-            AND Process = {process} 
-            AND Step = {step_name}
-            AND (Outcome = 'Done' OR Outcome = 'pass'))
+                AND Process = {process}
+                AND Step = {prev_step_name}
+                AND (Outcome = 'Done' OR Outcome = 'pass')
+                AND ScanCode NOT IN (SELECT 
+                    ScanCode
+                    FROM 
+                        ProcessingLog pl 
+                    WHERE Project = {project} 
+                        AND Process = {process} 
+                        AND Step = {step_name}
+                        AND (Outcome = 'Done' OR Outcome = 'pass'))
             """.format(project=dbprocs.format_string_parameter(project),
                        process=dbprocs.format_string_parameter(process),
                        step_name=dbprocs.format_string_parameter(step.step_name),
@@ -41,28 +41,19 @@ class DTIRepository(BaseRepository):
             ScanCode AS Code
         FROM 
             ProcessingLog pl 
-        WHERE 
-            Project = {0}
-        AND 
-            Process = 'dti' 
-        AND 
-            Step = 'Preregister'
-        AND 
-            Outcome = 'Done'
-        AND 
-            ScanCode NOT IN(
+        WHERE Project = {0}
+            AND Process = 'dti' 
+            AND Step = 'Preregister'
+            AND Outcome = 'Done'
+            AND ScanCode NOT IN(
                 SELECT 
                     ScanCode
                 FROM 
                     ProcessingLog pl 
-                WHERE 
-                    Project = {0}
-                AND 
-                    Process = 'dti' 
-                AND 
-                    Step = 'MaskQC'
-                AND 
-                    Outcome = 'pass'
+                WHERE Project = {0}
+                    AND Process = 'dti' 
+                    AND Step = 'MaskQC'
+                    AND Outcome = 'pass'
             )
         """.format(dbprocs.format_string_parameter(project))
 
@@ -77,26 +68,18 @@ class DTIRepository(BaseRepository):
             ProcessingLog pl 
         WHERE 
             Project = {0}
-        AND 
-            Process = 'dti' 
-        AND 
-            Step = 'TensorFit'
-        AND 
-            Outcome = 'Done'
-        AND 
-            ScanCode NOT IN(
+            AND Process = 'dti' 
+            AND Step = 'TensorFit'
+            AND Outcome = 'Done'
+            AND ScanCode NOT IN(
                 SELECT 
                     ScanCode
                 FROM 
                     ProcessingLog pl 
-                WHERE 
-                    Project = {0}
-                AND 
-                    Process = 'dti' 
-                AND 
-                    Step = 'WarpQC'
-                AND 
-                    Outcome = 'pass'
+                WHERE Project = {0}
+                    AND Process = 'dti' 
+                    AND Step = 'WarpQC'
+                    AND Outcome = 'pass'
             )
         """.format(dbprocs.format_string_parameter(project))
 
@@ -110,26 +93,18 @@ class DTIRepository(BaseRepository):
             ScanCode AS Code
         FROM 
             ProcessingLog pl 
-        WHERE 
-            Project = {0}
-        AND 
-            Process = 'dti' 
-        AND 
-            Step = 'MaskQC'
-        AND 
-            Outcome = 'Pass'
-        AND
-            ScanCode NOT IN (
-            SELECT 
-                ScanCode
-            FROM 
-                ProcessingLog
-            WHERE 
-                Project = {0}
-            AND 
-                Step = 'Register'
-            AND 
-                Outcome = 'Done'
+        WHERE Project = {0}
+            AND Process = 'dti' 
+            AND Step = 'MaskQC'
+            AND Outcome = 'Pass'
+            AND ScanCode NOT IN (
+                SELECT 
+                    ScanCode
+                FROM 
+                    ProcessingLog
+                WHERE Project = {0}
+                    AND Step = 'Register'
+                    AND Outcome = 'Done'
             )
         """.format(dbprocs.format_string_parameter(project))
 
@@ -143,26 +118,18 @@ class DTIRepository(BaseRepository):
             ScanCode AS Code
         FROM 
             ProcessingLog pl 
-        WHERE 
-            Project = {0}
-        AND 
-            Process = 'dti' 
-        AND 
-            Step = 'WarpQC'
-        AND 
-            Outcome = 'Pass'
-        AND
-            ScanCode NOT IN (
-            SELECT 
-                ScanCode
-            FROM 
-                ProcessingLog
-            WHERE 
-                Project = {0}
-            AND 
-                Step = 'RoiStats'
-            AND 
-                Outcome = 'Done'
+        WHERE Project = {0}
+            AND Process = 'dti' 
+            AND Step = 'WarpQC'
+            AND Outcome = 'Pass'
+            AND ScanCode NOT IN (
+                SELECT 
+                    ScanCode
+                FROM 
+                    ProcessingLog
+                WHERE Project = {0}
+                    AND Step = 'RoiStats'
+                    AND Outcome = 'Done'
             )
         """.format(dbprocs.format_string_parameter(project))
 
@@ -180,10 +147,8 @@ class DTIRepository(BaseRepository):
             dbo.DicomStudies st
         ON
             se.StudyInstanceUID = st.StudyInstanceUID
-        WHERE 
-            st.StudyCode = {project} 
-        AND 
-            SeriesDescription LIKE '%MR 2D AXIAL DTI BRAIN%'
+        WHERE st.StudyCode = {project} 
+            AND SeriesDescription LIKE '%MR 2D AXIAL DTI BRAIN%'
         """.format(project=fsp(project))
 
         todo = self.connection.fetchall(sql)
