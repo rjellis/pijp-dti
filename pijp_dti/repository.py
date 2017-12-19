f6ae038bb3205812308227de78d54fa81e267dee
Ryan Ellis <ryan.ellis@va.gov>
2017-12-19 12:47:56 -0800

    added set_roi_stats method

--- pijp_dti/repository.py
+++ pijp_dti/repository.py
@@ -1,5 +1,7 @@
+import csv
 from pijp.repositories import BaseRepository
 from pijp import dbprocs
+from pijp.dbprocs import format_string_parameter as fsp
 
 
 class DTIRepository(BaseRepository):
@@ -183,7 +185,25 @@ class DTIRepository(BaseRepository):
             st.StudyCode = {project} 
         AND 
             SeriesDescription LIKE '%MR 2D AXIAL DTI BRAIN%'
-        """.format(project=dbprocs.format_string_parameter(project))
+        """.format(project=fsp(project))
 
         todo = self.connection.fetchall(sql)
         return todo
+
+    def set_roi_stats(self, table, project, code, md, fa, ga, rd, ad):
+        sql = r"""INSERT INTO {table} VALUES ({code}, {project}, {fname}, {measure}, {roi}, {min}, {max}, {mean}, {sd})
+        """
+        measures = [md, fa, ga, rd, ad]
+        for m in measures:
+            with open(m) as csvfile:
+                mreader = csv.reader(csvfile, delimiter=',')
+                msr = fsp(m.split('_')[-2].rstrip('_roi.csv'))
+                for row in mreader:
+                    roi = fsp(str(row[0]))
+                    min = fsp(str(row[1]))
+                    max = fsp(str(row[2]))
+                    mean = fsp(str(row[3]))
+                    sd = fsp(str(row[4]))
+                    formatted_sql = sql.format(table=fsp(table), project=fsp(project), fname =fsp(m), code=fsp(code),
+                                               measure=msr, roi=roi, min=min, max=max, mean=mean, sd=sd)
+                    self.connection.execute_non_query(formatted_sql)
