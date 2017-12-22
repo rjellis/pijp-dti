f15ad62b9963646d14eedb1897fcc7b309af82b4
Ryan Ellis <ryan.ellis@va.gov>
2017-12-22 15:27:57 -0800

    changed path

--- tests/test_dti.py
+++ tests/test_dti.py
@@ -7,9 +7,10 @@ class Test(unittest.TestCase):
     def test_WarpQC(self):
 
         fa = '/m/InProcess/External/NRC/dti/NRC-FRA018-0003-V0-a1001/tenfit/NRC-FRA018-0003-V0-a1001_fa.nii.gz'
-        label = '/m/InProcess/External/NRC/dti/NRC-FRA018-0003-V0-a1001/tenfit/NRC-FRA018-0003-V0-a1001_warped_labels.nii.gz'
+        label = '/m/InProcess/External/NRC/dti/NRC-FRA018-0003-V0-a1001/warp/NRC-FRA018-0003-V0-a1001_warped_labels' \
+                '.nii.gz'
         code = 'NRC-FRA018-0003-V0-a1001'
 
-        warp_fig = dtiQC.get_warp_mosaic(fa, label, alpha=0.2)
+        warp_fig = dtiQC.get_warp_mosaic(fa, label, alpha=0.1)
         result, comment = QCinter.qc_tool(warp_fig, code)
         print(result)
