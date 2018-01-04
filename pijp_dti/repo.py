d636327929ec13850bfad4feed50666e005120ed
Ryan Ellis <ryan.ellis@va.gov>
2018-01-03 16:18:20 -0800

    added other columns

--- pijp_dti/repo.py
+++ pijp_dti/repo.py
@@ -126,8 +126,8 @@ class DTIRepository(BaseRepository):
     def set_roi_stats(self, project_id, code, md, fa, ga, rd, ad):
         sql = r"""
         INSERT INTO pijp_dti (Code, ProjectID, FileName, Measure, Roi, MinVal, MaxVal, MeanVal, StdDev, MedianVal, 
-        RecordTime)
-        VALUES ({code}, {project_id}, {fname}, {measure}, {roi}, {min}, {max}, {mean})
+        Volume, RecordTime)
+        VALUES ({code}, {project_id}, {fname}, {measure}, {roi}, {min}, {max}, {mean}, {sd}, {median}, {vol}, {time})
         """
         measures = [md, fa, ga, rd, ad]
         for m in measures:
@@ -146,9 +146,9 @@ class DTIRepository(BaseRepository):
                         median_val = fsp(str(row[4]))
                         volume = fsp(str(row[5]))
                         time = fsp(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-                        formatted_sql = sql.format(code=fsp(code), projectID=project_id, fname=fsp(m),
-                                                   measure=msr, roi=roi, min=min_val,
-                                                   max=max_val, mean=mean_val, sd=sd)
+                        formatted_sql = sql.format(code=fsp(code), projectID=project_id, measure=msr, roi=roi,
+                                                   min=min_val, max=max_val, mean=mean_val, sd=sd, median=median_val,
+                                                   vol=volume, time=time)
                         self.connection.execute_non_query(formatted_sql)
 
     def get_project_id(self, project):
