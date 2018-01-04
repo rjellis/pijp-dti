61c89304fd92dbf16c429df759b260d7b5ef3645
Ryan Ellis <ryan.ellis@va.gov>
2018-01-04 11:56:14 -0800

    version 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pijp-dti',
-    version='0.1.0',
+    version='0.2.0',
     author='Ryan Ellis',
     author_email='ellis2012ryan@gmail.com',
     packages=['pijp_dti',],
