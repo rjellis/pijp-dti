d1b33c8cb89436d574de7b0c0ce89e1203a5f3f0
Ryan Ellis <ryan.ellis@va.gov>
2018-01-04 11:56:42 -0800

    directories are now base step attributes

--- tests/test_dti.py
+++ tests/test_dti.py
@@ -9,25 +9,21 @@ class Test(unittest.TestCase):
     project = 'NRC'
     code = 'NRC-FRA018-0003-V0-a1001'
     args = None
-    working_dir = '/home/vhasfcellisr/tests/'
-    logdir = '/home/vhasfcellisr/tests'
     comments = 'testing'
 
     def test_stage(self):
 
         stage = dti.Stage(self.project, self.code, self.args)
-        stage.working_dir = self.working_dir
-        stage.logdir = self.logdir
         stage.run()
 
-        self.assertTrue(os.path.isdir(os.path.join(stage.working_dir, 'stage')))
-        self.assertTrue(os.path.isdir(os.path.join(stage.working_dir, 'register')))
-        self.assertTrue(os.path.isdir(os.path.join(stage.working_dir, 'denoise')))
-        self.assertTrue(os.path.isdir(os.path.join(stage.working_dir, 'mask')))
-        self.assertTrue(os.path.isdir(os.path.join(stage.working_dir, 'tenfit')))
-        self.assertTrue(os.path.isdir(os.path.join(stage.working_dir, 'warp')))
-        self.assertTrue(os.path.isdir(os.path.join(stage.working_dir, 'stats')))
-        self.assertTrue(os.path.isdir(os.path.join(stage.working_dir, 'qc')))
+        self.assertTrue(os.path.isdir(stage.stage_dir))
+        self.assertTrue(os.path.isdir(stage.reg_dir))
+        self.assertTrue(os.path.isdir(stage.den_dir))
+        self.assertTrue(os.path.isdir(stage.mask_dir))
+        self.assertTrue(os.path.isdir(stage.tenfit_dir))
+        self.assertTrue(os.path.isdir(stage.warp_dir))
+        self.assertTrue(os.path.isdir(stage.roiavg_dir))
+        self.assertTrue(os.path.isdir(stage.qc_dir))
         self.assertTrue(os.path.isfile(stage.fdwi))
         self.assertTrue(os.path.isfile(stage.fbval))
         self.assertTrue(os.path.isfile(stage.fbvec))
@@ -35,8 +31,6 @@ class Test(unittest.TestCase):
     def test_denoise(self):
 
         denoise = dti.Denoise(self.project, self.code, self.args)
-        denoise.working_dir = self.working_dir
-        denoise.logdir = self.logdir
         denoise.run()
 
         self.assertTrue(os.path.isfile(denoise.denoised))
@@ -44,8 +38,6 @@ class Test(unittest.TestCase):
     def test_register(self):
 
         register = dti.Register(self.project, self.code, self.args)
-        register.working_dir = self.working_dir
-        register.logdir = self.logdir
         register.run()
 
         self.assertTrue(os.path.isfile(register.b0))
@@ -55,8 +47,6 @@ class Test(unittest.TestCase):
     def test_mask(self):
 
         mask = dti.Mask(self.project, self.code, self.args)
-        mask.working_dir = self.working_dir
-        mask.logdir = self.logdir
         mask.run()
 
         self.assertTrue(os.path.isfile(mask.auto_mask))
@@ -65,8 +55,6 @@ class Test(unittest.TestCase):
     def test_apply_mask(self):
 
         apply_mask = dti.ApplyMask(self.project, self.code, self.args)
-        apply_mask.working_dir = self.working_dir
-        apply_mask.logdir = self.logdir
         apply_mask.run()
 
         self.assertTrue(os.path.isfile(apply_mask.masked))
@@ -74,8 +62,6 @@ class Test(unittest.TestCase):
     def test_tenfit(self):
 
         tenfit = dti.TensorFit(self.project, self.code, self.args)
-        tenfit.working_dir = self.working_dir
-        tenfit.logdir = self.logdir
         tenfit.run()
 
         self.assertTrue(os.path.isfile(tenfit.fa))
@@ -89,8 +75,6 @@ class Test(unittest.TestCase):
     def test_warp(self):
 
         warp = dti.Warp(self.project, self.code, self.args)
-        warp.working_dir = self.working_dir
-        warp.logdir = self.logdir
         warp.run()
 
         self.assertTrue(os.path.isfile(warp.warped_fa))
@@ -101,8 +85,6 @@ class Test(unittest.TestCase):
     def test_segment(self):
 
         seg = dti.Segment(self.project, self.code, self.args)
-        seg.working_dir = self.working_dir
-        seg.logdir = self.logdir
         seg.run()
 
         self.assertTrue(os.path.isfile(seg.segmented))
@@ -110,8 +92,6 @@ class Test(unittest.TestCase):
     def test_roistats(self):
 
         stats = dti.RoiStats(self.project, self.code, self.args)
-        stats.working_dir = self.working_dir
-        stats.logdir = self.logdir
         stats.run()
 
         self.assertTrue(os.path.isfile(stats.fa_roi))
