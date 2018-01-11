d5c6a208723b869d7f48fa61257ce8fc101ece5c
Ryan Ellis <ryan.ellis@va.gov>
2018-01-10 16:16:43 -0800

    changed code

--- tests/test_dti.py
+++ tests/test_dti.py
@@ -7,7 +7,7 @@ from pijp_dti import dti
 class Test(unittest.TestCase):
 
     project = 'NRC'
-    code = 'NRC-FRA018-0021-V0-a1001'
+    code = 'NRC-FRA018-0046-V0-a1201'
     args = None
     comments = 'testing'
 
