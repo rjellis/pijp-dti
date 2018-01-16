b56b223663bde33f7c7e48d0631acdd74ef6d425
Ryan Ellis <ryan.ellis@va.gov>
2018-01-16 12:32:19 -0800

    better method for chaging cases

--- tests/test_QCinter.py
+++ tests/test_QCinter.py
@@ -2,24 +2,15 @@ import unittest
 
 from pijp_dti import QCinter
 
-
-IMG_PATH = '/m/InProcess/External/NRC/pijp_dti/NRC-FRA018-0022-V0-a1201/4Tenfit/NRC-FRA018-0022-V0-a1201_fa.nii.gz'
-MSK_PATH = '/m/InProcess/External/NRC/pijp_dti/NRC-FRA018-0022-V0-a1201/6Segment/NRC-FRA018-0022-V0' \
-           '-a1201_warped_wm_labels' \
-           '.nii' \
-           '.gz'
-AUTO_PATH = '/m/InProcess/External/NRC/pijp_dti/NRC-FRA018-0022-V0-a1201/6Segment/NRC-FRA018-0022-V0' \
-            '-a1201_warped_wm_labels' \
-            '.nii' \
-            '.gz'
-
-CODE = 'NRC-FRA018-0022-V0-a1201'
-
+CODE = 'NRC-FRA018-0003-V0-a1001'
+IMG_PATH = '/m/InProcess/External/NRC/pijp_dti/{code}/2Register/{code}_b0.nii.gz'.format(code=CODE)
+MSK_PATH = '/m/InProcess/External/NRC/pijp_dti/{code}/3Mask/{code}_final_mask.nii.gz'.format(code=CODE)
+AUTO_PATH = '/m/InProcess/External/NRC/pijp_dti/{code}/3Mask/{code}_auto_mask.nii.gz'.format(code=CODE)
 
 class Test(unittest.TestCase):
 
     def test_qc_tool(self):
-        QCinter.run_qc_interface(CODE, IMG_PATH, AUTO_PATH, MSK_PATH, mosaic_mode=True)
+        QCinter.run_qc_interface(CODE, IMG_PATH, AUTO_PATH, MSK_PATH)
 
 
 if __name__ == "__main__":
