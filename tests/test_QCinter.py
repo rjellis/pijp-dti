1afd2a0bcb9938892427d94a6268dd4b67ae404b
Ryan Ellis <ryan.ellis@va.gov>
2018-01-29 10:46:37 -0800

    fixed path

--- tests/test_QCinter.py
+++ tests/test_QCinter.py
@@ -3,9 +3,9 @@ import unittest
 from pijp_dti import QCinter
 
 CODE = 'NRC-FRA018-0003-V0-a1001'
-IMG_PATH = '/m/InProcess/External/NRC/pijp-dti-dev/{code}/2Register/{code}_b0.nii.gz'.format(code=CODE)
-MSK_PATH = '/m/InProcess/External/NRC/pijp-dti-dev/{code}/3Mask/{code}_final_mask.nii.gz'.format(code=CODE)
-AUTO_PATH = '/m/InProcess/External/NRC/pijp-dti-dev/{code}/3Mask/{code}_auto_mask.nii.gz'.format(code=CODE)
+IMG_PATH = '/m/InProcess/External/NRC/pijp-dti/{code}/2Register/{code}_b0.nii.gz'.format(code=CODE)
+MSK_PATH = '/m/InProcess/External/NRC/pijp-dti/{code}/3Mask/{code}_final_mask.nii.gz'.format(code=CODE)
+AUTO_PATH = '/m/InProcess/External/NRC/pijp-dti/{code}/3Mask/{code}_auto_mask.nii.gz'.format(code=CODE)
 
 class Test(unittest.TestCase):
 
