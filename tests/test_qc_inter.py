b17d7fcb3e382d4ee5232938519c7906131b5a9f
Ryan Ellis <ryan.ellis@va.gov>
2018-02-06 16:06:12 -0800

    comment

--- tests/test_qc_inter.py
+++ tests/test_qc_inter.py
@@ -2,10 +2,10 @@ import unittest
 
 from pijp_dti import qc_inter
 
-CODE = 'SampleCode'
-IMG_PATH = 'Sample/Path/To/Image'
-MSK_PATH = 'Sample/Path/To/Mask'
-AUTO_PATH = 'Sample/Path/To/AutoMask'
+CODE = 'NRC-FRA018-0003-V0'
+IMG_PATH = '/m/InProcess/External/NRC/pijp-dti-dev/{code}/2Register/{code}_b0.nii.gz'.format(code=CODE)
+MSK_PATH = '/m/InProcess/External/NRC/pijp-dti-dev/{code}/3Mask/{code}_final_maks.nii.gz'.format(code=CODE)
+AUTO_PATH = '/m/InProcess/External/NRC/pijp-dti-dev/{code}/3Mask/{code}_auto_mask.nii.gz'.format(code=CODE)
 
 class Test(unittest.TestCase):
 
