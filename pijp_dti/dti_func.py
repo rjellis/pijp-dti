cfd9d65d108054503055a36066748cdb6968386e
Ryan Ellis <ryan.ellis@va.gov>
2018-03-19 10:17:51 -0700

    docstrings

--- pijp_dti/dti_func.py
+++ pijp_dti/dti_func.py
@@ -18,15 +18,16 @@ def mask(dat):
     num_pass is the number of passes for the median filter
 
     Args:
-        dat (ndarray): 3D numpy ndarray.
+        dat (ndarray): 3D image.
 
     Returns:
-        dat (ndarray): The skull stripped image as an ndarray.
+        bin_mask (ndarray): The skull stripped image.
 
     """
 
     rescaled_dat = rescale(dat)
-    mask_dat, bin_dat = otsu.median_otsu(rescaled_dat, median_radius=2, numpass=4, dilate=2)
+    mask_dat, bin_dat = otsu.median_otsu(
+        rescaled_dat, median_radius=2, numpass=4, dilate=2)
     bin_mask = fill_holes(bin_dat)
     bin_mask = extract_largest_component(bin_mask)
 
@@ -37,11 +38,11 @@ def apply_mask(dat, bin_mask):
     """Apply the binary mask.
 
     Args:
-        dat (ndarray): The image to be masked
-        bin_mask (ndarray): boolean mask
+        dat (ndarray): The image to be masked.
+        bin_mask (ndarray): boolean mask.
 
     Returns:
-        masked (ndarray): The image with the mask applied
+        masked (ndarray): The image with the mask applied.
 
     """
     return otsu.applymask(dat, bin_mask)
@@ -51,10 +52,10 @@ def denoise(dat):
     """Denoise a data set using Non Local Means.
 
     Args:
-        dat (ndarray): 3D or 4D numpy ndarray
+        dat (ndarray): 3D or 4D image.
 
     Returns:
-        denoise_dat (ndarray): The denoised ndarray
+        denoise_dat (ndarray): denoised image.
 
     """
     sigma = noise_estimate.estimate_sigma(dat)
@@ -62,8 +63,11 @@ def denoise(dat):
 
     if len(dat.shape) == 4:
         for i in range(0, dat.shape[3]):
-            denoise_dat.append(non_local_means.non_local_means(dat[..., i], sigma[i]))
+            denoise_dat.append(
+                non_local_means.non_local_means(dat[..., i], sigma[i]))
+
         denoise_dat = np.stack(denoise_dat, axis=-1)
+
     elif len(dat.shape) == 3:
         denoise_dat = non_local_means.non_local_means(dat, sigma)
 
@@ -76,9 +80,9 @@ def denoise_pca(dat, bval, bvec):
     Local PCA is a denoising method specific to 4D diffusion weighted images.
 
     Args:
-        dat (ndarray): 3D or 4D numpy ndarray
-        bval (ndarray): 1D ndarray containing the b-values for the DWI directions
-        bvec (ndarray): 2D ndarray containing the b-vectors for the DWI directions
+        dat (ndarray): 3D or 4D image.
+        bval (ndarray): 1D ndarray containing the b-values
+        bvec (ndarray): 2D ndarray containing the b-vectors
 
     Returns:
         denoise_dat (ndarray): The denoised ndarray
@@ -94,12 +98,12 @@ def average_b0(dat, aff, bval):
     """Obtain the average b0 image from a 4D DWI image.
 
     Args:
-        dat (ndarray): 4D ndarray of a diffusion weighted image.
-        aff (ndarray): 4 X 4 ndarray of the affine of the image.
-        bval (ndarray): 1D ndarray containing the b-values for the DWI directions
+        dat (ndarray): 4D diffusion weighted image.
+        aff (ndarray): 4 X 4 affine of the image.
+        bval (ndarray): 1D ndarray containing the b-values
 
     Returns:
-        b0 (ndarray): 3D ndarray of the averaged b0 image.
+        b0 (ndarray): 3D averaged b0 image.
 
     """
     b0 = None
@@ -109,7 +113,8 @@ def average_b0(dat, aff, bval):
         if bval[i] == 0:
             if b0 is None:
                 b0 = dat[..., i]
-            b0_reg, b0_aff, b0_map = affine_registration(b0, dat[..., i], aff, aff, rigid=True)
+            b0_reg, b0_aff, b0_map = affine_registration(
+                b0, dat[..., i], aff, aff, rigid=True)
             b0s_reg.append(b0_reg)
 
     b0s_reg = np.stack(b0s_reg, axis=-1)
@@ -118,16 +123,16 @@ def average_b0(dat, aff, bval):
     return b0_avg
 
 
-def register(b0, dat, b0_aff, aff, bval, bvec):
+def register(b0, dwi, b0_aff, aff, bval, bvec):
     """Rigidly register a 4D DWI to its own 3D b0 image.
 
     Args:
-        b0 (ndarray): 3D ndarray of the stacked b0 image.
-        dat (ndarray): 4D array of the DWI image containing b0 and diffusion weighted directions
+        b0 (ndarray): 3D average b0 image.
+        dwi (ndarray): 4D DWI image
         b0_aff (ndarray): 4 X 4 affine matrix for the b0
         aff (ndarray): 4 X 4 affine matrix for the DWI
-        bval (ndarray): 1D ndarray containing the b-values for the DWI directions
-        bvec (ndarray): 1D ndarray containing the b-vectors for the DWI directions
+        bval (ndarray): 1D ndarray containing the b-values
+        bvec (ndarray): 2D ndarray containing the b-vectors
 
     Returns:
         reg_dat (ndarray): 4D ndarray of the registered DWI
@@ -138,12 +143,13 @@ def register(b0, dat, b0_aff, aff, bval, bvec):
     reg_map = []
     reg_dat = []
 
-    for i in range(0, dat.shape[3]):
-        reg_dir, reg_aff, reg_map_slice = affine_registration(b0, dat[..., i], b0_aff, aff, rigid=True)
+    for i in range(0, dwi.shape[3]):
+        reg_dir, reg_aff, reg_map_slice = affine_registration(
+            b0, dwi[..., i], b0_aff, aff, rigid=True)
         reg_dat.append(reg_dir)
         reg_map.append(reg_map_slice.affine)
-        if bval[i] != 0:
-            affines.append(reg_aff)  # Only want to update b-vectors with non-zero b-values
+        if bval[i] != 0: # Only want to update b-vectors with non-zero b-values
+            affines.append(reg_aff)
 
     reg_dat = np.stack(reg_dat, axis=-1)
     reg_map = np.stack(reg_map, axis=-1)
@@ -158,14 +164,14 @@ def fit_dti(dat, bval, bvec):
     """Fit the tensor using the Weighted Least Squares fit method.
 
     Args:
-        dat (ndarray): 4D ndarray of the diffusion weighted image.
-        bval (ndarray): 1D ndarray containing the b-values for the DWI directions.
-        bvec (ndarray): 2D ndarray containing the b-vectors for the DWI directions.
+        dat (ndarray): 4D diffusion weighted image.
+        bval (ndarray): 1D ndarray containing the b-values
+        bvec (ndarray): 2D ndarray containing the b-vectors
 
     Returns:
-        evals (ndarray): 4D ndarray of the eigenvalues for every voxel
-        evecs (ndarray): 5D ndarray of the eigenvectors for every voxel
-        tenfit (TensorFit object): TensorFit object containing the evals, evecs and other DTI measures
+        evals (ndarray): 4D ndarray of the eigenvalues
+        evecs (ndarray): 5D ndarray of the eigenvectors
+        tenfit (TensorFit object): TensorFit dipy object
 
     """
     gtab = gradients.gradient_table(bval, bvec)
@@ -181,10 +187,10 @@ def segment_tissue(dat):
     """Segments the image into different tissue classifications
 
     Args:
-        dat (ndarray): 3D ndarray
+        dat (ndarray): 3D image
 
     Returns:
-          pve (ndarray): 4D ndarray where the 4th dimension is the class of the tissue
+          pve (ndarray): 4D ndarray where the 4th dimension is the tissue class
     """
 
     nclass = 4
@@ -192,7 +198,7 @@ def segment_tissue(dat):
 
     hmrf = TissueClassifierHMRF()
     hmrf.verbose = False
-    initial_segmentation, final_segmentation, pve = hmrf.classify(dat, nclass, beta)
+    initial_seg, final_seg, pve = hmrf.classify(dat, nclass, beta)
 
     return pve
 
@@ -217,17 +223,16 @@ def apply_tissue_mask(dat, segmented, tissue=1, prob=50):
 
 
 def roi_stats(dat, overlay, labels, zooms):
-    """Get statistics on a diffusion tensor measure in specific regions of interest.
+    """Get statistics on a diffusion tensor measure in specific roi's
 
     Args:
-        dat (ndarray): 3D ndarray of an diffusion tensor measurement such as: Fractional Anisotropy,
-                       Mean Diffusivity, etc.
-        overlay (ndarray): 3D ndarray with the same dimension of dat.
-        labels (Union): A dictionary of overlay values as keys that correspond to region labels_lookup as dictionary values.
+        dat (ndarray): 3D image of an diffusion tensor measurement.
+        overlay (ndarray): 3D label image.
+        labels (Union): A dictionary of overlay values as keys that correspond
+                        to region labels_lookup as dictionary values.
         zooms (list): list of voxel dimensions
     Returns:
-        stats (list): Array of the list of stats for all regions of interests. Lists are in the format: [name, min, max,
-                      mean, standard deviation].
+        stats (list): Array of the list of stats for all regions of interests.
 
     """
     roi_voxels = dict()
@@ -241,14 +246,14 @@ def roi_stats(dat, overlay, labels, zooms):
         if key in labels:  # Find the label name for that integer value
             roi_name = labels[key]
             if val != 0:
-                roi_voxels[roi_name].append(val)  # Add the intensity of the voxel to the list for the ROI
+                roi_voxels[roi_name].append(val)
 
     for rois in roi_voxels.keys():
         npa = np.asarray(roi_voxels[rois])
         try:
             # Name, Min, Max, Mean, Std. Dev, Median, Volume
-            stats.append([rois, npa.min(), npa.max(), npa.mean(), npa.std(), np.median(npa),
-                          (len(npa)*vox_size)])
+            stats.append([rois, npa.min(), npa.max(), npa.mean(),
+                          npa.std(), np.median(npa), (len(npa)*vox_size)])
 
         except ValueError:  # Catches rows with empty ROIs
             stats.append([rois, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
@@ -256,32 +261,34 @@ def roi_stats(dat, overlay, labels, zooms):
     return stats
 
 
-def affine_registration(static, moving, static_affine, moving_affine, rigid=False):
+def affine_registration(static, moving,
+                        static_affine, moving_affine, rigid=False):
     """Register one 3D array to another using linear registration.
 
     Args:
-        static (ndarray): 3D ndarray that is being used as reference.
-        moving (ndarray): 3D ndarray that is being registered.
+        static (ndarray): 3D image used as  reference
+        moving (ndarray): 3D image to register
         static_affine (ndarray): 4 X 4 affine matrix of the static image.
         moving_affine (ndarray): 4 X 4 affine matrix of the moving image.
-        rigid (bool): A selector for rigid registration instead of affine registration.
+        rigid (bool): A flag for rigid registration
 
     Returns:
 
-        dat_reg (ndarray): The registered moving 3D ndarray.
-        rigid.affine (ndarray): 4 X 4 affine matrix of the registered image if rigid is selected.
-        affine.affine (ndarray): 4 X 4 affine matrix of the registered image if affine is selected.
+        dat_reg (ndarray): The 3D registered moving image.
+        affine (ndarray): 4 X 4 transformation matrix
 
     """
     # Mutual information Metric
     nbins = 32  # Number of bins for computing the histograms
     sampling_prop = 100  # percentage of voxels (0, 100]
     metric = imaffine.MutualInformationMetric(nbins, sampling_prop)
-    level_iters = [10000, 1000, 100]  # Iterations at each resolution (coarse to fine)
+    level_iters = [10000, 1000, 100]  # coarse to fine
     sigmas = [3.0, 1.0, 0.0]
     factors = [4, 2, 1]  # Factors that determine resolution
 
-    affreg = imaffine.AffineRegistration(metric, level_iters=level_iters, sigmas=sigmas, factors=factors, verbosity=0)
+    affreg = imaffine.AffineRegistration(
+        metric, level_iters=level_iters, sigmas=sigmas,
+        factors=factors, verbosity=0)
     params0 = None
 
     if rigid:
@@ -290,7 +297,9 @@ def affine_registration(static, moving, static_affine, moving_affine, rigid=Fals
         transform = transforms.AffineTransform3D()
 
     # 'mass' tells optimize to align the image's centers of mass
-    aff_map = affreg.optimize(static, moving, transform, params0, static_affine, moving_affine, 'mass')
+    aff_map = affreg.optimize(static, moving,
+                              transform, params0,
+                              static_affine, moving_affine, 'mass')
     dat_reg = aff_map.transform(moving)
 
     return dat_reg, aff_map.affine, aff_map
@@ -300,24 +309,26 @@ def sym_diff_registration(static, moving, static_affine, moving_affine):
     """Register one 3D array to another using non-linear registration.
 
     Args:
-        static (ndarray): 3D ndarray that is being used as reference.
-        moving (ndarray): 3D ndarray that is being registered.
+        static (ndarray): 3D image used as reference.
+        moving (ndarray): 3D image to register
         static_affine (ndarray): 4 X 4 affine matrix of the static image.
         moving_affine (ndarray): 4 X 4 affine matrix of the moving image.
 
     Returns:
-        warped_moving (ndarray): The registered moving 3D ndarray
-        mapping (DiffeomorphicMap): The mapping object to warp 3D ndarrays to the obtained registration
+        warped_moving (ndarray): The registered moving 3D image
+        mapping (DiffeomorphicMap): The mapping object
 
     """
-    moving_reg, pre_align, reg_map = affine_registration(static, moving, static_affine, moving_affine)
+    moving_reg, pre_align, reg_map = affine_registration(
+        static, moving, static_affine, moving_affine)
     metric = metrics.CCMetric(3)  # Cross Correlation Metric for 3 dimensions
     level_iters = [100, 100, 30]  # Iterations at each resolution (fine
     #  to coarse)
 
     sdr = imwarp.SymmetricDiffeomorphicRegistration(metric, level_iters)
     sdr.verbosity = 0
-    mapping = sdr.optimize(static, moving, static_affine, moving_affine, pre_align)
+    mapping = sdr.optimize(
+        static, moving, static_affine, moving_affine, pre_align)
     warped_moving = mapping.transform(moving)
 
     return warped_moving, mapping
